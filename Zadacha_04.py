5c1f0aacd31ddd43d2c8e9f1d632f60b00143d03
Birdick <birdick@mail.ru>
2023-04-10 14:21:26 +0300

    Done

--- Zadacha_04.py
+++ Zadacha_04.py
@@ -7,11 +7,16 @@
 # 24 -> 4 16 4
 # 60 -> 10 40 10
 
-peter = int(input("How many birds has made Peter: "))
-sergey = peter
-print (f"Sergey has made {sergey}")
-kate = (peter + sergey)*2
-print (f"Kate has made {kate}")
-total = peter + sergey + kate
-print (f"Guys have made together {total} birds")
+# peter = int(input("How many birds has made Peter: "))
+# sergey = peter
+# print (f"Sergey has made {sergey}")
+# kate = (peter + sergey)*2
+# print (f"Kate has made {kate}")
+# total = peter + sergey + kate
+# print (f"Guys have made together {total} birds")
 
+summ = int(input ("How many birds have made children? "))
+part = summ//6
+print(f"Peter made {part} birds")
+print(f"Sergey made {part} birds")
+print(f"Kate made {part*4} bifds")
